3fbd6f1b9717c029015840e84af9c3774fb00d87
AnhLe1194 <lekimkieuanh@gmail.com>
2020-10-08 09:39:54 -0700

    next commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,33 +2,32 @@
 //     // console.log(data)
 //     var dropdownMenu = d3.select("#selDataset");
 //     var names = data.names
-//     var samples = data.samples
 //     // console.log(samples)
 //     names.forEach(name => {
 //     dropdownMenu.append('option').text(name)  
 //     });
-//     // charts(names[0])
+//     charts(names[0])
 
 // });
 
-// // function optionChanged(newname) {
-// //     charts(newname)
-// // }
+// function optionChanged(newname) {
+//     charts(newname)
+// }
 
-// // function charts(name) {
-// //     console.log(name)
-// //     d3.json("samples.json").then(function(data){
-// //         var samples = data.samples
-// //         console.log(samples)
-// //     })
-// // }
+// function charts(name) {
+//     console.log(name)
+//     d3.json("samples.json").then(function(data){
+//         var samples = data.samples
+//         console.log(samples)
+//     })
+// }
 
 function init() {
-    // Select the dropdown element
-    var selector = d3.select("#selDataset");
     
     // Populate the dropdown with subject ID's from the list of sample Names
       d3.json("samples.json").then((data) => {
+
+        // Select the dropdown element
         var dropdownMenu = d3.select("#selDataset");
         var ids = data.names
         ids.forEach(id => {
@@ -36,39 +35,11 @@ function init() {
         });
         
       // Use the first ID from the names to build initial plots
-        charts(ids[0])
+      const firstSubject = ids[0];
+      updateCharts(firstSubject);
+      updateMetadata(firstSubject);
     });
+}
 
-    
-        function optionChanged(newid) {
-        charts(newid)
-        };
-
-        function charts(id) {
-        d3.json("samples.json").then(function(data){
-        var samples = data.samples
-        // console.log(samples)
-        
-
-
-        })
-    };
-
-
-
-
-
-
-
-
-
-
-
-  }
-  
-  
-  
-  
-  
-  // Initialize the dashboard
-  init();
+init();
+ 
